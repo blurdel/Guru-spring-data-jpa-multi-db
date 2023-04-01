4e046b68c5e2f5a5abedc9ce1b05b589d8154978
blurdel <5648737+blurdel@users.noreply.github.com>
2023-04-01 16:28:51 -0400

    Added lombok

--- src/main/java/com/blurdel/sdjpa/multidb/domain/pan/CreditCardPAN.java
+++ src/main/java/com/blurdel/sdjpa/multidb/domain/pan/CreditCardPAN.java
@@ -3,10 +3,22 @@ package com.blurdel.sdjpa.multidb.domain.pan;
 import com.blurdel.sdjpa.multidb.domain.CreditCardConverter;
 
 import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CreditCardPAN {
 
 	@Id
