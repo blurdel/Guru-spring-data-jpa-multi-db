af8192748a663aabb0033759ce0c7f0c204ee520
blurdel <5648737+blurdel@users.noreply.github.com>
2023-04-03 18:57:27 -0400

    Added Transient properties that wint be persisted

--- src/main/java/com/blurdel/sdjpa/multidb/domain/creditcard/CreditCard.java
+++ src/main/java/com/blurdel/sdjpa/multidb/domain/creditcard/CreditCard.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -30,5 +31,17 @@ public class CreditCard {
 
     @Convert(converter = CreditCardConverter.class)
     private String expirationDate;
+
+    @Transient
+    private String creditCardNumber;
+
+    @Transient
+    private String firstName;
+
+    @Transient
+    private String lastName;
+
+    @Transient
+    private String zipCode;
     
 }
